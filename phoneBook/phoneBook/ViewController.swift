d4c8472f6dd7be69b979192dcdf3f37d2c963e56
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-05-19 07:56:00 +0800

    initialize the proj

---
+++ phoneBook/phoneBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phoneBook
+//
+//  Created by Chialin Liu on 2020/5/19.
+//  Copyright © 2020 Chialin Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
