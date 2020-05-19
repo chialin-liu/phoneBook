9dccc589031c66bb91c8fa9e16a60c1708b12d67
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-05-19 20:52:39 +0800

    setup cell layout

--- phoneBook/phoneBook/ViewController.swift
+++ phoneBook/phoneBook/ViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    let cellId = "cellId"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        tableView.backgroundColor = UIColor(red: 10/255, green: 40/255, blue: 60/255, alpha: 1)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+//        tableView.separatorStyle = .none
+        tableView.separatorColor = .white
+        //purpose: to set a blank view at the cellview bottom
+        tableView.tableFooterView = UIView()
         setupNavigationBar()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Plus", style: .plain, target: self, action: #selector(handlePlus))
         navigationItem.rightBarButtonItem?.tintColor = .white
+    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .lightGray
+        return view
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
     }
     @objc func handlePlus() {
         print("PLUS")
@@ -28,8 +42,16 @@ class ViewController: UIViewController {
         //        navigationController?.navigationBar.isTranslucent = false
                 //Be aware
                 //instead of using navigationController?.navigationItem.title
-                navigationItem.title = "PhoneBook"
-                navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+            navigationItem.title = "PhoneBook"
+            navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .systemTeal
+        return cell
     }
 }
 
